97d76a2ea51f36f712d20aeff525fbf3f2077b2c
rafabrodrigues <rafaelbarbosarodrigues47@gmail.com>
2024-06-03 05:45:03 -0300

    Criando Input pesquisar na screen Avisos

--- src/screens/AvisoScreen/index.jsx
+++ src/screens/AvisoScreen/index.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useRef } from 'react';
+import { View, Text, TextInput, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
 import styles from './styles';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function AvisoScreen() {
+  const searchRef = useRef(null);
+
+  const handleSearchPress = () =>{
+    if(searchRef.current){
+      searchRef.current.focus();
+    };
+  };
+
   return (
+
+
     <View style={styles.container}>
-      <Text style={styles.text}>Avisos</Text>
+      <View style={styles.mainContainer}>
+        <View style={styles.searchContainer}>
+          <TouchableOpacity style={styles.searchIcon} onPress={handleSearchPress}>
+            <AntDesign name="search1" size={24} color="#fff" />
+          </TouchableOpacity>
+          <TextInput
+            style={styles.searchInput}
+            ref={searchRef}
+            placeholder='Pesquisar'
+            placeholderTextColor='#edf2f4'
+          />
+        </View>
+      </View>
     </View>
   );
 }
--- src/screens/AvisoScreen/styles.jsx
+++ src/screens/AvisoScreen/styles.jsx
@@ -1,17 +1,33 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor:'#2b2d42',
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center'
+    container: {
+        backgroundColor: '#edf2f4',
+        flex: 1,
+        alignItems: 'center',
     },
-    text:{
-        color: '#fff',
-        fontSize:20,
-        fontWeight:'800'
+    mainContainer: {
+        flex: 1,
+        width: '90%',
+        justifyContent: 'center',
+    },
+    searchContainer: {
+        backgroundColor: '#2d2b42',
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderRadius:4,
+    },
+    searchIcon: {
+        paddingLeft:12,
+    },
+    searchInput: {
+        padding: 8,
+        fontFamily:'Poppins_400Regular',
+        color:'#fff',
+        width:'100%',
+        fontSize:16,
     }
+
 });
 
 export default styles;
